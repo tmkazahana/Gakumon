489c59e9cc49f9f05742be6cf1be05de28ccfc66
tmkazahana <kazahana.517@gmail.com>
2025-09-05 20:54:07 +0900

    ハンバーガーメニュー追加

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,21 +8,67 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
+      
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Text(
+                'メニュー',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.book),
+              title: const Text('図鑑'),
+              onTap: () {
+                Navigator.pop(context); // ドロワーを閉じる
+                // TODO: 図鑑ページへ遷移
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.restaurant), // 仮の胃アイコン
+              title: const Text('胃'),
+              onTap: () {
+                Navigator.pop(context);
+                // TODO: 胃ページへ遷移
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('設定'),
+              onTap: () {
+                Navigator.pop(context);
+                // TODO: 設定ページへ遷移
+              },
+            ),
+          ],
+        ),
+      ),
+      // --- AppBar ---
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+       
+        iconTheme: IconThemeData(
+          size: 36, 
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+      // --- Body ---
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
               'assets/images/logo1.gif',
-              width: 150,
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'メニュー',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+              width: 200,
+            ),                      
             const SizedBox(height: 40),
             ElevatedButton(
               onPressed: () {
